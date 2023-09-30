71445f63b65c036f8ede0768ce0d098915660f37
Gabriel <karlakpas@gmail.com>
2023-09-29 22:14:10 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,46 +1,34 @@
-const input_pass = document.getElementById("input1")
-const input_car = document.getElementById("input2")
-const button = document.getElementById("btn")
-const div = document.getElementById("div")
+const input_pass = document.getElementById("input1");
+const input_car = document.getElementById("input2");
+const encryptButton = document.getElementById("encrypt-btn");
+const decryptButton = document.getElementById("decrypt-btn");
+const encryptedDiv = document.getElementById("encrypted-div");
+const decryptedDiv = document.getElementById("decrypted-div");
 
-button.addEventListener("click", ()=>{
-    let password = String(input_pass.value)
-    let caracters = String(input_car.value)
+encryptButton.addEventListener("click", () => {
+  const password = input_pass.value;
+  const characters = input_car.value;
+  const encryptedPassword = encryptPassword(password, characters);
+  encryptedDiv.innerText = encryptedPassword;
+});
 
-    div.innerText = execute(password, caracters)
-})
+decryptButton.addEventListener("click", () => {
+    const password = input_pass.value;
+    const characters = input_car.value;
+    const decryptedPassword = decryptPassword(encryptPassword(password, characters), characters);
+    decryptedDiv.innerText = decryptedPassword;
+  });
 
-function to_array(length){
-    let push_to = []
-    for(i in length){
-        push_to.push(length[i])
-    }
-    console.log(push_to)
-    return push_to
+function encryptPassword(password, characters) {
+  let encryptedPassword = "";
+  for (i in password) {
+    encryptedPassword += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
+  }
+  return encryptedPassword;
 }
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-}
-
-function to_cripto(pass, caracters){
-    
-    for (i in pass){
-        pass[i] = caracters[getRandomInt(caracters.length - 1)]
-    }
-    console.log(pass)
-    return pass
-}
-
-function to_string(string) {
-    let str = []
-    for (i in string){
-        str += string[i]
-    }
-    console.log(str)
-    return str
-}
-
-function execute(term1, term2) {
-    return to_string(to_cripto(to_array(term1), to_array(term2)))
+function decryptPassword() {
+  return Password;
 }
